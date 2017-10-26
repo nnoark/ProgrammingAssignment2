0756e3d5492adf4558047070419448040308bc08
nnoark <32202870+nnoark@users.noreply.github.com>
2017-10-26 18:29:12 -0400

    makeCacheMatrix function completed

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,20 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions will create a special invertible matrix, find its inverse,
+## and then the result is cached to be called upon later
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+                I <- NULL
+                set <- function(y) {
+                  x <<- y
+                  I <<- NULL
+                }
+                get <- function() x
+                setinverse <- function(solve) I <<- inverse
+                getinverse <- function() I
+                list(set = set, get = get, 
+                    setinverse = setinverse, getinverse = getinverse)
+                
 }
 
 
